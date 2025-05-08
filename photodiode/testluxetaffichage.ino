a2b6001ef6b790118dbedff8e81ab372198f0b57
swyftos <98902001+swyftos@users.noreply.github.com>
2025-05-08 17:38:31 +0200

    Update testluxetaffichage.ino
    affichage avec 2 leds

--- photodiode/testluxetaffichage.ino
+++ photodiode/testluxetaffichage.ino
@@ -5,12 +5,17 @@ LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 
 const int capteur1 = A0;
 const int capteur2 = A1;
+const int led1 = 6; // LED 1
+const int led2 = 7; // LED 2
 
 void setup() {
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Capteurs Lumiere");
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
@@ -20,21 +25,26 @@ void loop() {
   float tension1 = val1 * 5.0 / 1023.0;
   float tension2 = val2 * 5.0 / 1023.0;
 
-  // Ligne 1 : capteur 1
+  // Affichage sur l'écran
   lcd.setCursor(0, 0);
   lcd.print("A0:");
   lcd.print(val1);
   lcd.print(" ");
-  lcd.print(tension1, 1); // 1 chiffre après virgule
-  lcd.print("V");
+  lcd.print(tension1, 1);
+  lcd.print("V ");
 
-  // Ligne 2 : capteur 2
   lcd.setCursor(0, 1);
   lcd.print("A1:");
   lcd.print(val2);
   lcd.print(" ");
   lcd.print(tension2, 1);
-  lcd.print("V");
+  lcd.print("V ");
 
-  delay(500);
+  // Clignotement LED 1 et LED 2 en opposition
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  delay(250);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  delay(250);
 }
